23694277cfdfc77fa0b7fa36e9ee0ad899a678e1
effine <zhang.yafei@foundbyte.com>
2024-06-17 09:30:28 +0800

    初始化

---
+++ src/main/java/cn/effine/template/maven/Main.java
@@ -0,0 +1,13 @@
+package cn.effine.template.maven;
+
+
+/**
+ * @author effine
+ * @email zhang.yafei#gzdzswy.cn
+ * @date 2024-06-17 09:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
