c637330d1accfc00038fbd6a3e763d81560e1356
effine <zhang.yafei@foundbyte.com>
2024-06-17 09:45:23 +0800

    修改邮箱

--- src/main/java/cn/effine/algorithm/Main.java
+++ src/main/java/cn/effine/algorithm/Main.java
@@ -1,9 +1,9 @@
-package cn.effine.template.maven;
+package cn.effine.algorithm;
 
 
 /**
  * @author effine
- * @email zhang.yafei#gzdzswy.cn
+ * @email iballad#163.com
  * @date 2024-06-17 09:24
  */
 public class Main {
